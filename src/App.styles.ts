e79515f48cc362d510ab88a5cf1e329b9cb49082
unknown <lamaceno2005@gmail.com>
2022-10-27 23:11:35 -0300

    atualização

---
+++ src/App.styles.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+
+ export const Container = styled.div ``;
+
+ export const Hedear = styled.div `
+ background-color:#1E90FF;
+ height:150px;
+ text-align:center;
+ 
+ 
+ `;
+
+ export const HeaderText = styled.h1 `
+  margin:0;
+  padding:0;
+  color:#fff;
+  padding-top:30px;
+
+ `;
+
+ export const Body = styled.div `
+ margin:auto;
+ max-with:980px;
+ background-color:red;
+ 
+ `;
+
+
+
---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import * as C from './App.styles'
+
+const App = () => {
+  return (
+    <C.Container>
+      <C.Hedear>
+        <C.HeaderText>
+          Word List
+        </C.HeaderText>
+      </C.Hedear>
+      <C.Body>
+        .....
+        {/* Area informação */}
+        {/* Area irsesao */}
+        {/* Tabela */}
+
+
+      </C.Body>
+    </C.Container>
+
+
+
+  )
+
+
+}
+
+export default App;
